cb900bf25690ff9430797b64832f867fa385f8ed
LuukvdBilt <338255@student.mboutrecht.nl>
2025-11-05 10:48:50 +0100

    Nog meeer aanpassingen

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // root van de main branch
+  base: '/', // root van de branch
 })
+
 
 
