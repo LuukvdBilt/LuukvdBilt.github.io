c949636811c55d7c297707dc05ed27c44e818fac
LuukvdBilt <338255@student.mboutrecht.nl>
2025-11-05 10:46:08 +0100

    nog meer aanpassingen

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/LuukPokemonFE.github.io/', // <- dit zorgt dat alle assets correct geladen worden
+  base: '/', // root van de main branch
 })
+
 
