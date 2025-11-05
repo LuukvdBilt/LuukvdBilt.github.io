4308115d259d73c72059d9443a63cefc693133e8
LuukvdBilt <338255@student.mboutrecht.nl>
2025-11-05 11:04:12 +0100

    Diepe zucht

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/LuukPokemonFE.github.io/'
+  base: '/LuukPokemonFE.github.io/', // <— heel belangrijk!
 })
-
-
 
